27381e57d5ada2ba7ff4cb22663ac9d23900c60c
Javiersanjuan1 <fsanjua@myuax.com>
2024-02-22 11:32:14 +0100

    Initial commit

--- Sistema_de_Alerta_y_Monitoreo.java
+++ Sistema_de_Alerta_y_Monitoreo.java
@@ -42,7 +42,7 @@ class SistemaMonitoreo {
     }
 
     public void generarPrimos() {
-        int n = 1000; // Se puede ajustar según la cantidad
+        int n = 1000; // Se puede ajustar
         primos = new int[n];
         primos[0] = 2;
         int count = 1;
